14967152688227dfb7c4135dd7f4784103fab18a
Kaya Tacer <ktacer@proton.me>
2025-04-12 16:22:35 -0400

    Remove videos after extraction

--- src/load_data.py
+++ src/load_data.py
@@ -2,6 +2,7 @@ import kagglehub
 from pathlib import Path
 import process_video
 import os
+import shutil
 
 DATASET_NAME = "rakshitgirish/golf-pose"
 SUBPATH = "CUSTOM DATASET/SIDE VIEW"
@@ -15,15 +16,17 @@ def process_data(data_path: str) -> None:
     for root, _, files in os.walk(data_path):
         for file in files:
             bw_video = process_video.convert_video_to_grayscale(root, file)
-            
+
             if bw_video is not None:
                 relative_path = os.path.relpath(root, data_path)
                 output_path = Path(FRAMES_OUTPUT) / Path(relative_path) / file[:-4]
                 process_video.extract_frames(bw_video, output_path)
 
 def run() -> None:
-    data_path = Path(download_data()) / Path(SUBPATH)
+    full_dataset_path = download_data()
+    data_path = Path(full_dataset_path) / Path(SUBPATH)
     process_data(data_path)
+    shutil.rmtree(full_dataset_path)
 
 if __name__ == "__main__":
     run()
