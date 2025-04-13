2658b0d771b74d125237e2d7ae51a878e67f6499
Kaya Tacer <ktacer@proton.me>
2025-04-13 02:05:34 -0400

    Update pose_data_loader.py

--- src/pose_data_loader.py
+++ src/pose_data_loader.py
@@ -9,13 +9,14 @@ class PoseData(Dataset):
     def __init__(self, root_dir):
         self.root_dir = root_dir
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.input = sorted([f for f in os.listdir(self.root_dir) if f.startswith("data") and f.endswith(".mat")])
+        self.labels = sorted([f for f in os.listdir(self.root_dir) if f.startswith("metric") and f.endswith(".mat")])
 
     def __len__(self) -> int:
-        return self.input.shape[0]
+        return len(self.input)
 
     def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
-        mat_data = loadmat(os.path.join(self.root_dir, f"data_{str(idx).zfill(3)}.mat"))
-
-        label_data = loadmat(os.path.join(self.root_dir, f"metric_{str(idx).zfill(3)}.mat"))
+        mat_data = loadmat(self.input[idx])
+        label_data = loadmat(self.labels[idx])
 
         return torch.tensor(mat_data, device=self.device), torch.tensor(label_data, device=self.device)
