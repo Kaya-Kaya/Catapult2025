dfa3f6e26ef1b6201489c85b1d9170f68b65dc2a
Kaya Tacer <ktacer@proton.me>
2025-04-13 02:02:10 -0400

    Update pose_data_loader.py

--- src/pose_data_loader.py
+++ src/pose_data_loader.py
@@ -6,31 +6,16 @@ from scipy.io import loadmat
 from typing import Tuple
 
 class PoseData(Dataset):
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir):
         self.root_dir = root_dir
-        self.transform = transform
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    def load(self) -> None:
-        self.input = []
-        self.labels = []
-        
-        for root, _, files in os.walk(self.root_dir):
-            for file in files:
-                if file.endswith('.mat'):
-                    mat_data = loadmat(os.path.join(root, file))
-                    self.input.append(mat_data)
-
-                    index_str = file[file.rindex("_"):-4]
-                    label_data = loadmat(os.path.join(root, f"metric_{index_str}.mat"))
-                    self.labels.append(label_data)
-
-        self.input = torch.tensor(self.input, device=self.device)
-        self.labels = torch.tensor(self.labels, device=self.device)
 
     def __len__(self) -> int:
         return self.input.shape[0]
-    
 
     def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
-        return self.input[idx], self.labels[idx]
+        mat_data = loadmat(os.path.join(self.root_dir, f"data_{str(idx).zfill(3)}.mat"))
+
+        label_data = loadmat(os.path.join(self.root_dir, f"metric_{str(idx).zfill(3)}.mat"))
+
+        return torch.tensor(mat_data, device=self.device), torch.tensor(label_data, device=self.device)
